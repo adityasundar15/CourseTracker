3391cbb2046e86359859e838cc0ecd6fbf4da90a
Y.H LIEN <sean.0916413240@gmail.com>
2024-05-09 17:09:54 +0900

    firebase connection update

--- src/firebase-config.tsx
+++ src/firebase-config.tsx
@@ -9,13 +9,20 @@ import { getFirestore } from 'firebase/firestore';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  // apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  // authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  // projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  // storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  // messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  // appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  // measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  apiKey: 'AIzaSyBvr8kKOABj3M5OULsRXP6r7eBStGKSBm8',
+  authDomain: 'coursetracker-ec6c5.firebaseapp.com',
+  projectId: 'coursetracker-ec6c5',
+  storageBucket: 'coursetracker-ec6c5.appspot.com',
+  messagingSenderId: '475184291215',
+  appId: '1:475184291215:web:6df2b6c4fdb8b183b6fffa',
+  measurementId: 'G-ZL3L3MYVRC',
 };
 
 // Initialize Firebase
--- src/pages/Test.tsx
+++ src/pages/Test.tsx
@@ -2,54 +2,65 @@ import { useState, useEffect } from 'react';
 import { db } from '/Users/y.h.lien/Desktop/Github/CourseTracker/src/firebase-config.tsx';
 import {
   collection,
-  onSnapshot,
+  // getDocs,
   addDoc,
   updateDoc,
-  doc,
   deleteDoc,
+  doc,
+  onSnapshot,
 } from 'firebase/firestore';
 
 const Test = () => {
   const [newName, setNewName] = useState('');
   const [newAge, setNewAge] = useState(0);
+
   const [users, setUsers] = useState([]);
   const usersCollectionRef = collection(db, 'users');
 
-  // Function to add a new user to Firestore
   const createUser = async () => {
     await addDoc(usersCollectionRef, { name: newName, age: Number(newAge) });
   };
 
-  // Function to update the age of a user in Firestore
-  const updateUser = async (id, age) => {
+  const increaseUserAge = async (id, age) => {
     const userDoc = doc(db, 'users', id);
-    const newField = { age: age + 1 };
-    await updateDoc(userDoc, newField);
+    const newFields = { age: age + 1 };
+    await updateDoc(userDoc, newFields);
   };
 
-  // Function to delete a user from Firestore
+  const decreaseUserAge = async (id, age) => {
+    const userDoc = doc(db, 'users', id);
+    const newFields = { age: age - 1 };
+    await updateDoc(userDoc, newFields);
+  };
+
   const deleteUser = async (id) => {
     const userDoc = doc(db, 'users', id);
     await deleteDoc(userDoc);
   };
 
+  // useEffect(() => {
+  //   const getUsers = async () => {
+  //     const data = await getDocs(usersCollectionRef);
+  //     setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+  //   };
+
+  //   getUsers();
+  // }, []);
+
   useEffect(() => {
-    // Function to fetch initial users and set up real-time updates
     const unsubscribe = onSnapshot(usersCollectionRef, (snapshot) => {
-      const updatedUsers = [];
-      snapshot.forEach((doc) => {
-        updatedUsers.push({ ...doc.data(), id: doc.id });
-      });
+      const updatedUsers = snapshot.docs.map((doc) => ({
+        ...doc.data(),
+        id: doc.id,
+      }));
       setUsers(updatedUsers);
     });
 
-    // Clean up function to unsubscribe from real-time updates when component unmounts
-    return () => unsubscribe();
-  }, []); // Empty dependency array to run only once when component mounts
+    return () => unsubscribe(); // Unsubscribe from the listener when the component unmounts
+  }, []);
 
   return (
     <div
-      className="Test"
       style={{
         display: 'flex',
         justifyContent: 'center',
@@ -57,25 +68,23 @@ const Test = () => {
         height: '100vh',
       }}
     >
-      <div style={{ textAlign: 'center' }}>
+      <div className="App">
         <input
           placeholder="Name..."
           onChange={(event) => {
             setNewName(event.target.value);
           }}
-        ></input>
-        <br />
+        />
         <input
           type="number"
           placeholder="Age..."
           onChange={(event) => {
-            // @ts-expect-error: Ignore type error since event.target.value is expected to be a string
-            setNewAge(event.target.value);
+            // setNewAge(event.target.value);
+            setNewAge(parseFloat(event.target.value));
           }}
-        ></input>
-        <br />
-        <button onClick={createUser}>Create User</button>
-        <br />
+        />
+
+        <button onClick={createUser}> Create User</button>
         {users.map((user) => {
           return (
             <div key={user.id}>
@@ -83,10 +92,17 @@ const Test = () => {
               <h1>Age: {user.age}</h1>
               <button
                 onClick={() => {
-                  updateUser(user.id, user.age);
+                  increaseUserAge(user.id, user.age);
                 }}
               >
                 Increase Age
+              </button>
+              <button
+                onClick={() => {
+                  decreaseUserAge(user.id, user.age);
+                }}
+              >
+                Decrease Age
               </button>
               <button
                 onClick={() => {
@@ -104,9 +120,3 @@ const Test = () => {
 };
 
 export default Test;
-
-// const Test = () => {
-//   return <div className="App"></div>;
-// };
-
-// export default Test;
