d6d98d35a640689847e83b9391f18b5c760b8bec
Y.H LIEN <sean.0916413240@gmail.com>
2024-07-25 01:53:23 +0900

    auto category adding function v1

--- src/firestoreUtils.tsx
+++ src/firestoreUtils.tsx
@@ -1,24 +1,24 @@
-import { doc, setDoc } from "firebase/firestore";
-import { CourseCategory } from "./pages/Courses";
-import { auth, db } from "./firebase-config";
+import { doc, setDoc } from 'firebase/firestore';
+import { CourseCategory } from './pages/Courses';
+import { auth, db } from './firebase-config';
 
 export const updateCourseCategoriesInFirestore = async (
-  updatedCategories: CourseCategory[]
+  updatedCategories: CourseCategory[],
 ) => {
   const currentUser = auth.currentUser;
   if (currentUser) {
     try {
-      const userDocRef = doc(db, "users", currentUser.uid);
+      const userDocRef = doc(db, 'users', currentUser.uid);
       await setDoc(
         userDocRef,
         { Categories: updatedCategories },
         { merge: true },
       );
-      console.log("Categories updated in Firestore.");
+      console.log('Categories updated in Firestore.');
     } catch (error) {
-      console.error("Error updating categories in Firestore:", error);
+      console.error('Error updating categories in Firestore:', error);
     }
   } else {
-    console.log("No user is currently logged in.");
+    console.log('No user is currently logged in.');
   }
 };
