6e7d6bda5e24ee313a59cfabbec11527225724a4
Y.H LIEN <sean.0916413240@gmail.com>
2024-07-26 16:33:29 +0900

    fetch data at login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,11 +1,11 @@
-import "../css/Login.css";
+import '../css/Login.css';
 
-import { useEffect, useState } from "react";
-import { Button, Col, Row } from "react-bootstrap";
-import { useNavigate } from "react-router-dom";
-import { auth, db, signinWithGoogle } from "../firebase-config";
-import { collection, doc, setDoc } from "firebase/firestore";
-import { FaArrowRight } from "react-icons/fa6";
+import { useEffect, useState } from 'react';
+import { Button, Col, Row } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
+import { auth, db, signinWithGoogle } from '../firebase-config';
+import { collection, doc, getDoc, setDoc } from 'firebase/firestore';
+import { FaArrowRight } from 'react-icons/fa6';
 
 interface UserInfo {
   displayName: string;
@@ -16,18 +16,18 @@ interface UserInfo {
 function Login() {
   const navigate = useNavigate();
   const [currentUser, setCurrentUser] = useState<UserInfo | null>(null);
-  const usersCollectionRef = collection(db, "users");
+  const usersCollectionRef = collection(db, 'users');
 
   const navigateHome = () => {
-    navigate("/");
+    navigate('/');
   };
 
   const navigateCourses = () => {
-    navigate("/courses");
+    navigate('/courses');
   };
 
   const navigateProfile = () => {
-    navigate("/profile");
+    navigate('/profile');
   };
 
   const handleSigninWithGoogle = async () => {
@@ -49,15 +49,46 @@ function Login() {
           {
             uid: currentUser.uid,
           },
-          { merge: true }
+          { merge: true },
         );
+        await fetchSelectedCourses(currentUser.uid);
       } else {
-        console.log("User logged out, resetting state...");
+        console.log('User logged out, resetting state...');
         setCurrentUser(null);
       }
     });
     return () => unsubscribe();
   }, []);
+
+  const fetchSelectedCourses = async (uid: string) => {
+    try {
+      console.log('Fetching selected courses for user:', uid);
+      const userDocRef = doc(db, 'users', uid);
+      const userDoc = await getDoc(userDocRef);
+      if (userDoc.exists()) {
+        const userData = userDoc.data();
+        console.log('User data from API:', userData); // Log raw user data
+
+        // Replace local storage with API data
+        const apiCategories = userData?.Categories || [];
+        localStorage.setItem('courseCategories', JSON.stringify(apiCategories));
+      } else {
+        localStorage.removeItem('courseCategories'); // Clear local storage if no data exists in API
+      }
+
+      const storedCategories = localStorage.getItem('courseCategories');
+      if (storedCategories) {
+        console.log(
+          'Updated stored categories from local storage:',
+          JSON.parse(storedCategories),
+        );
+      } else {
+        console.log('No stored categories in local storage.');
+      }
+    } catch (error) {
+      console.error('Error fetching selected courses: ', error);
+    }
+  };
 
   return (
     <div id="parent-container" className="vh-100 d-flex flex-column">
@@ -84,7 +115,7 @@ function Login() {
                   >
                     <span
                       className="px-4 py-1"
-                      style={{ fontFamily: "Outfit", fontWeight: 300 }}
+                      style={{ fontFamily: 'Outfit', fontWeight: 300 }}
                     >
                       Profile
                     </span>
@@ -95,15 +126,15 @@ function Login() {
                     className="d-flex justify-content-center align-items-center"
                     variant="dark"
                     style={{
-                      backgroundColor: "#000000",
-                      borderRadius: "30px",
-                      fontSize: "1.5rem",
+                      backgroundColor: '#000000',
+                      borderRadius: '30px',
+                      fontSize: '1.5rem',
                     }}
                     onClick={navigateCourses}
                   >
                     <span
                       className="px-4 py-1"
-                      style={{ fontFamily: "Outfit", fontWeight: 300 }}
+                      style={{ fontFamily: 'Outfit', fontWeight: 300 }}
                     >
                       Go to Courses <FaArrowRight />
                     </span>
@@ -126,15 +157,15 @@ function Login() {
                   className="d-flex justify-content-center align-items-center"
                   variant="dark"
                   style={{
-                    backgroundColor: "#000000",
-                    borderRadius: "17px",
-                    fontSize: "1.5rem",
+                    backgroundColor: '#000000',
+                    borderRadius: '17px',
+                    fontSize: '1.5rem',
                   }}
                   onClick={handleSigninWithGoogle}
                 >
                   <span
                     className="px-4 py-1"
-                    style={{ fontFamily: "Outfit", fontWeight: 300 }}
+                    style={{ fontFamily: 'Outfit', fontWeight: 300 }}
                   >
                     Continue with Google
                   </span>
